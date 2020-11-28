0e9bd3e5052ab2e465f9ed0aa06d76276b2c605c
Trevor Elliott <awesomelyawesome@gmail.com>
2020-11-27 20:12:31 -0800

    Fix build_compdb.sh
    Actually rebuild the compile commands db

--- tools/scripts/build_compdb.sh
+++ tools/scripts/build_compdb.sh
@@ -9,4 +9,6 @@ root="$(dirname "${BASH_SOURCE[0]}")/../.."
 outfile="$(bazel info bazel-bin)/compile_commands.json"
 execroot="$(bazel info execution_root)"
 
+bazel build //:aoc_compdb
+
 sed "s|__EXEC_ROOT__|${execroot}|" "$outfile" > "$root/compile_commands.json"
