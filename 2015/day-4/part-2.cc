a18646332eea3900ceea7d11c8dbbe0471b46e23
Trevor Elliott <awesomelyawesome@gmail.com>
2020-12-06 12:43:17 -0800

    Cut out a second on 2015 day 4 part 2

--- 2015/day-4/part-2.cc
+++ 2015/day-4/part-2.cc
@@ -7,41 +7,74 @@
 
 using namespace std;
 
-string md5(string input) {
+struct count_zeros{
+
+    bool only_zeros;
+    int zeros;
+
+    count_zeros() : only_zeros{true}, zeros{0} {}
+
+    count_zeros &operator*() {
+        return *this;
+    }
+
+    count_zeros &operator++() {
+        return *this;
+    }
+
+    const count_zeros &operator++(int) {
+        return *this;
+    }
+
+    count_zeros &operator=(const char c) {
+        if (only_zeros && c == '0') {
+            ++zeros;
+        } else {
+            only_zeros = false;
+        }
+        return *this;
+    }
+};
+
+template <>
+struct std::iterator_traits<count_zeros> {
+    using iterator_category = std::output_iterator_tag;
+    using value_type = void;
+};
+
+int md5_leading_zeros(boost::uuids::detail::md5 hash, const string &input) {
     using boost::uuids::detail::md5;
 
-    md5 hash;
     hash.process_bytes(input.data(), input.size());
 
     md5::digest_type digest;
     hash.get_digest(digest);
 
-    string result;
     const auto intDigest = reinterpret_cast<const int *>(&digest);
-    boost::algorithm::hex(intDigest,
+
+    auto zeros = boost::algorithm::hex(intDigest,
                           intDigest + (sizeof(md5::digest_type) / sizeof(int)),
-                          back_inserter(result));
+                          count_zeros{});
 
-    return result;
-}
-
-int leading_zeros(string digest) {
-    using namespace ranges;
-    return ranges::count(
-        digest | views::take_while([](auto c) { return c == '0'; }), '0');
+    return zeros.zeros;
 }
 
 int main(int argc, char **argv) {
     using namespace ranges;
 
-    auto res = views::iota(1) | views::transform([](auto i) -> pair<int, int> {
-                   auto digest = md5("iwrupvqb" + std::to_string(i));
-                   return {i, leading_zeros(digest)};
-               }) |
-               views::drop_while([](auto p) { return p.second != 6; }) |
-               views::take(1) | ranges::to<vector<pair<int, int>>>();
+    boost::uuids::detail::md5 hash;
+    hash.process_bytes("iwrupvqb", 8);
 
-    fmt::print("part-2: {}\n", res.front().first);
+    string input;
+    input.reserve(20);
+
+    for (auto i : views::iota(1)) {
+        input = to_string(i);
+        if (md5_leading_zeros(hash, input) == 6) {
+            fmt::print("part-2: {}\n", i);
+            break;
+        }
+    }
 
     return 0;
 }
