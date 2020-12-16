37b7cf39e9419d890a8f5dda315ca852b5adaadf
Trevor Elliott <awesomelyawesome@gmail.com>
2020-12-15 22:49:17 -0800

    Format, and some cleanup

--- 2020/day-16/part-2.cc
+++ 2020/day-16/part-2.cc
@@ -37,7 +37,7 @@ int main(int argc, char **argv) {
     vector<rule> rules{};
 
     vector<int> me{};
-    vector<vector<int>> others{};
+    vector<vector<int>> valid{};
 
     {
         ifstream in{argv[1]};
@@ -62,46 +62,47 @@ int main(int argc, char **argv) {
 
             case 2:
                 for (auto const &line : lines | views::drop(1)) {
-                    auto &ticket = others.emplace_back();
-                    for (auto &match :
-                         line | views::tokenize(regex{"[[:digit:]]+"})) {
-                        ticket.emplace_back(stoi(match));
+                    auto ticket = line |
+                                  views::tokenize(regex{"[[:digit:]]+"}) |
+                                  views::transform([](auto const &str) {
+                                      return stoi(str);
+                                  }) |
+                                  to<vector>();
+
+                    if (ranges::all_of(ticket, [&rules](auto n) {
+                            return ranges::any_of(rules, [n](auto const &rule) {
+                                return rule.valid(n);
+                            });
+                        })) {
+                        valid.emplace_back(std::move(ticket));
                     }
                 }
             }
         }
     }
 
-    auto valid = others | views::filter([&rules](auto const &ticket) {
-                     return ranges::all_of(ticket, [&rules](auto n) {
-                         return ranges::any_of(rules, [n](auto const &rule) {
-                             return rule.valid(n);
-                         });
-                     });
-                 }) |
-                 to<vector>();
-
     // rule index to potential columns
     vector<vector<int>> constraints(rules.size(), vector<int>{});
-
-    // collect constraints
     for (auto const &p : rules | views::enumerate) {
         auto ix = p.first;
         auto &rule = p.second;
         for (auto col : views::ints(0, static_cast<int>(constraints.size()))) {
             if (ranges::all_of(valid, [col, &rule](auto const &ticket) {
-                            return rule.valid(ticket[col]);
-                        })) {
+                    return rule.valid(ticket[col]);
+                })) {
                 constraints[ix].emplace_back(col);
             }
         }
     }
 
-    // experimentally, there are no guesses to make when solving the constraint set
     vector<int> solution(rules.size(), -1);
     while (true) {
-        auto it = ranges::find_if(constraints, [](auto const &cs) { return cs.size() == 1; });
+        auto it = ranges::find_if(
+            constraints, [](auto const &cs) { return cs.size() == 1; });
         if (it == constraints.end()) {
+            // experimentally, there are no guesses to make when solving the
+            // constraint set, so it's fine to quit out when there are no
+            // singleton sets left
             break;
         }
 
@@ -113,15 +114,17 @@ int main(int argc, char **argv) {
         }
     }
 
-    auto res = ranges::accumulate(
-        rules | views::enumerate | views::transform([&solution, &me](auto const &p) {
-            auto &[ix, rule] = p;
-            if (rule.name.substr(0, 9) == "departure") {
-                return static_cast<int64_t>(me[solution[ix]]);
-            } else {
-                return 1L;
-            }
-        }), 1L, ranges::multiplies{});
+    auto res =
+        ranges::accumulate(views::zip_with(
+                               [&me](auto const &rule, auto col) {
+                                   if (rule.name.substr(0, 9) == "departure") {
+                                       return static_cast<int64_t>(me[col]);
+                                   } else {
+                                       return 1L;
+                                   }
+                               },
+                               rules, solution),
+                           1L, ranges::multiplies{});
 
     fmt::print("part 2: {}\n", res);
 
