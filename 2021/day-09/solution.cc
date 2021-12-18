131b617c27672a1cb3ec4c7ca808ddbf6c1da2f6
Trevor Elliott <awesomelyawesome@gmail.com>
2021-12-18 14:46:30 -0800

    Port 2021 day-09 to common

--- 2021/day-09/solution.cc
+++ 2021/day-09/solution.cc
@@ -6,116 +6,65 @@
 #include <string>
 #include <vector>
 
+#include "common/pos.h"
+
 namespace views = ranges::views;
 
 struct cardinal_positions;
-
-struct pos {
-    int x;
-    int y;
-
-    pos(int x, int y) : x{x}, y{y} {}
-
-    cardinal_positions cardinals() const;
-
-    bool operator==(const pos &other) const {
-        return this->x == other.x && this->y == other.y;
-    }
-
-    bool operator<(const pos &other) const {
-        if (this->x == other.x) {
-            return this->y < other.y;
-        } else {
-            return this->x < other.x;
-        }
-    }
-};
-
-struct cardinal_positions {
-    pos north;
-    pos east;
-    pos south;
-    pos west;
-};
-
-cardinal_positions pos::cardinals() const {
-    return cardinal_positions{
-        pos{this->x, this->y - 1},
-        pos{this->x + 1, this->y},
-        pos{this->x, this->y + 1},
-        pos{this->x - 1, this->y},
-    };
-}
 
 int main(int argc, char **argv) {
     if (argc != 2) {
         return 0;
     }
 
-    std::vector<std::vector<int>> grid;
+    aoc::bounds b;
+    std::vector<int> grid;
     {
         std::ifstream in{argv[1]};
         for (auto &&line : ranges::getlines(in)) {
-            auto &row = grid.emplace_back();
+            b.width = line.size();
+            b.height++;
             for (auto c : line) {
-                row.push_back(c - '0');
+                grid.push_back(c - '0');
             }
         }
     }
 
     int risk_level = 0;
-    std::vector<pos> low_points;
-    for (const auto &[y, row] : views::enumerate(grid)) {
-        for (auto [x, i] : views::enumerate(row)) {
-            if (x > 0 && row[x - 1] <= i) {
-                continue;
-            }
+    std::vector<aoc::pos> low_points;
+    for (auto &&row : b.rows()) {
+        for (auto p : row) {
+            auto i = grid[b.index(p)];
 
-            if (x + 1 < row.size() && row[x + 1] <= i) {
-                continue;
+            if (ranges::all_of(b.cardinals(p), [i, b, &grid](auto d) {
+                    auto j = grid[b.index(d)];
+                    return j > i;
+                })) {
+                low_points.emplace_back(p);
+                risk_level += i + 1;
             }
-
-            if (y > 0 && grid[y - 1][x] <= i) {
-                continue;
-            }
-
-            if (y + 1 < grid.size() && grid[y + 1][x] <= i) {
-                continue;
-            }
-
-            low_points.emplace_back(x, y);
-            risk_level += i + 1;
         }
     }
 
     fmt::print("part 1: {}\n", risk_level);
 
-    auto lookup = [&grid](pos p) -> int {
-        if (p.x < 0 || p.x >= grid.front().size() || p.y < 0 || p.y >= grid.size()) {
-            return 9;
-        }
-        return grid[p.y][p.x];
-    };
-
-    auto floodfill = [&lookup](pos &start) {
-        std::set<pos> seen;
-        std::vector<std::pair<pos, int>> work{{start, -1}};
+    auto floodfill = [&grid, b](aoc::pos &start) {
+        std::set<aoc::pos> seen;
+        std::vector<std::pair<aoc::pos, int>> work{{start, -1}};
 
         while (!work.empty()) {
             auto [p, i] = work.back();
             work.pop_back();
 
-            auto j = lookup(p);
+            auto j = grid[b.index(p)];
             if (j == 9 || j <= i || seen.find(p) != seen.end()) {
                 continue;
             }
 
             seen.insert(p);
-            auto [n, e, s, w] = p.cardinals();
-            work.emplace_back(n, j);
-            work.emplace_back(e, j);
-            work.emplace_back(s, j);
-            work.emplace_back(w, j);
+            for (auto d : b.cardinals(p)) {
+                work.emplace_back(d, j);
+            }
         }
 
         return seen.size();
--- common/pos.h
+++ common/pos.h
@@ -3,8 +3,11 @@
 
 #include <cstddef>
 #include <range/v3/view/cartesian_product.hpp>
+#include <range/v3/view/concat.hpp>
+#include <range/v3/view/filter.hpp>
 #include <range/v3/view/iota.hpp>
 #include <range/v3/view/remove.hpp>
+#include <range/v3/view/single.hpp>
 #include <range/v3/view/transform.hpp>
 
 namespace aoc {
@@ -18,6 +21,31 @@ struct pos final {
 
     bool operator==(const pos &other) const {
         return this->x == other.x && this->y == other.y;
+    }
+
+    // lexicographic ordering on x, y
+    bool operator<(const pos &other) const {
+        if (this->x == other.x) {
+            return this->y < other.y;
+        } else {
+            return this->x < other.x;
+        }
+    }
+
+    pos north() const {
+        return pos{this->x, this->y - 1};
+    }
+
+    pos east() const {
+        return pos{this->x + 1, this->y};
+    }
+
+    pos south() const {
+        return pos{this->x, this->y + 1};
+    }
+
+    pos west() const {
+        return pos{this->x - 1, this->y};
     }
 };
 
@@ -45,6 +73,23 @@ struct bounds final {
                });
     }
 
+    auto rows() const {
+        auto ys = ranges::views::iota(0, this->height);
+        return ys | ranges::views::transform([this](auto y) {
+                   return ranges::views::iota(0, this->width) |
+                          ranges::views::transform([y](auto x) { return pos(x, y); });
+               });
+    }
+
+    auto cardinals(pos p) const {
+        return ranges::views::concat(
+                   ranges::views::single(p.north()),
+                   ranges::views::single(p.east()),
+                   ranges::views::single(p.south()),
+                   ranges::views::single(p.west())) |
+               ranges::views::filter([this](auto d) { return this->in_bounds(d); });
+    }
+
     auto neighbors(pos p) const {
         auto xs = ranges::views::iota(std::max(p.x - 1, 0), std::min(this->width, p.x + 2));
         auto ys = ranges::views::iota(std::max(p.y - 1, 0), std::min(this->height, p.y + 2));
@@ -53,6 +98,10 @@ struct bounds final {
                    return pos{x, y};
                }) |
                ranges::views::remove(p);
+    }
+
+    bool in_bounds(pos p) const {
+        return p.x >= 0 && p.x < this->width && p.y >= 0 && p.y < this->height;
     }
 };
 
