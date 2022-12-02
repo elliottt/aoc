0fd0c8f1bf344f23dddedb576a1f3dbba38fadd7
Trevor Elliott <awesomelyawesome@gmail.com>
2022-12-01 21:01:11 -0800

    2022 day 1

---
+++ 2022/day-01/solution.cc
@@ -0,0 +1,28 @@
+#include <fmt/format.h>
+#include <fstream>
+#include <range/v3/all.hpp>
+#include <string>
+#include <vector>
+
+namespace views = ranges::views;
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        return 1;
+    }
+
+    std::vector<int> totals;
+    {
+        std::ifstream in{argv[1]};
+        totals =
+            ranges::getlines(in) | views::split("") | views::transform([](auto &&rng) {
+                return ranges::accumulate(rng | views::transform([](std::string &str) { return std::stoi(str); }), 0);
+            }) |
+            ranges::to<std::vector> | ranges::actions::sort | ranges::actions::reverse;
+    }
+
+    fmt::print("part 1: {}\n", totals[0]);
+    fmt::print("part 2: {}\n", ranges::accumulate(totals | views::take(3), 0));
+
+    return 0;
+}
