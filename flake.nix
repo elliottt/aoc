d528e8214ec007f12efc92ea10634ed97ee4c827
Trevor Elliott <awesomelyawesome@gmail.com>
2023-12-02 21:04:53 -0800

    Use a nix flake for the env

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Development environment for elliottt/aoc";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            clang
+            bazelisk
+          ];
+        };
+      }
+    );
+}
